1ff6ba26ca8666d0261fc3603bef2e7f2aabf482
Skymirrh <skymirrh@gmail.com>
2018-01-01 18:35:05 +0100

    Add NullableBoolToBoolConverter

---
+++ CompatibilityManager/Converters/NullableBoolToBoolConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace CompatibilityManager.Converters
+{
+    public class NullableBoolToBoolConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((bool?)value) ?? false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool?)value;
+        }
+
+        private static NullableBoolToBoolConverter converter;
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return converter ?? (converter = new NullableBoolToBoolConverter());
+        }
+    }
+}
