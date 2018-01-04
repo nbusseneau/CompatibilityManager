d1d119ee15edfd80f895f3306ff2e15f5811ced7
Skymirrh <skymirrh@gmail.com>
2018-01-04 14:20:11 +0100

    Fix d3d9.dll fault crash due to RivaTuner Statistic Server trying to inject itself into WPF apps

--- CompatibilityManager/Views/MainWindow.xaml.cs
+++ CompatibilityManager/Views/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -24,6 +25,9 @@ namespace CompatibilityManager
         public MainWindow()
         {
             InitializeComponent();
+
+            // Fix d3d9.dll fault crash due to RivaTuner Statistic Server trying to inject itself into WPF apps 
+            RenderOptions.ProcessRenderMode = RenderMode.SoftwareOnly;
         }
     }
 }
