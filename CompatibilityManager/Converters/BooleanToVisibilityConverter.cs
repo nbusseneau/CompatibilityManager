f4b68929f51f6a154481c7f3da0beee04e192db7
Skymirrh <skymirrh@gmail.com>
2018-01-05 04:39:04 +0100

    Add BooleanToVisibilityConverter

---
+++ CompatibilityManager/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CompatibilityManager.Converters
+{
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public class BooleanToVisibilityConverter : BaseConverter<BooleanToVisibilityConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (Visibility)value == Visibility.Visible ? true : false;
+        }
+    }
+}
