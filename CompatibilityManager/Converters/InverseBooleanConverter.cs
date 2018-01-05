9b008670ebd2e075a744a32b1477bc04beb9af6d
Skymirrh <skymirrh@gmail.com>
2018-01-05 04:39:13 +0100

    Add InverseBooleanConverter

---
+++ CompatibilityManager/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CompatibilityManager.Converters
+{
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class InverseBooleanConverter : BaseConverter<InverseBooleanConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+    }
+}
