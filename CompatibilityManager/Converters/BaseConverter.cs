afe193b2ad6388cd5e58a43ce1e233767a56b3a2
Skymirrh <skymirrh@gmail.com>
2018-01-05 04:38:55 +0100

    Add BaseConverter

---
+++ CompatibilityManager/Converters/BaseConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace CompatibilityManager.Converters
+{
+    [MarkupExtensionReturnType(typeof(IValueConverter))]
+    public abstract class BaseConverter<T> : MarkupExtension, IValueConverter
+        where T : class, IValueConverter, new()
+    {
+        public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+        public abstract object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture);
+
+        private static T converter;
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return converter ?? (converter = new T());
+        }
+    }
+}
--- CompatibilityManager/Converters/NullableBoolToBoolConverter.cs
+++ CompatibilityManager/Converters/NullableBoolToBoolConverter.cs
@@ -1,26 +1,20 @@
 ﻿using System;
 using System.Globalization;
 using System.Windows.Data;
-using System.Windows.Markup;
 
 namespace CompatibilityManager.Converters
 {
-    public class NullableBoolToBoolConverter : MarkupExtension, IValueConverter
+    [ValueConversion(typeof(bool?), typeof(bool))]
+    public class NullableBoolToBoolConverter : BaseConverter<NullableBoolToBoolConverter>
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return ((bool?)value) ?? false;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return (bool?)value;
-        }
-
-        private static NullableBoolToBoolConverter converter;
-        public override object ProvideValue(IServiceProvider serviceProvider)
-        {
-            return converter ?? (converter = new NullableBoolToBoolConverter());
         }
     }
 }
