6587bf8d3ee13b556b1158ea37484bc97c2dd92b
Skymirrh <skymirrh@gmail.com>
2018-02-08 21:48:41 +0100

    Add AdditionalFlagsViewModel

---
+++ CompatibilityManager/ViewModels/AdditionalFlagViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Events;
+using Prism.Mvvm;
+
+namespace CompatibilityManager.ViewModels
+{
+    public class FlagChanged : PubSubEvent { }
+
+    public class AdditionalFlagViewModel : BindableBase
+    {
+        private EventAggregator eventAggregator;
+
+        private string flag; 
+        public string Flag
+        {
+            get => this.flag;
+            set => SetProperty(ref this.flag, value, this.eventAggregator.GetEvent<FlagChanged>().Publish);
+        }
+
+        public AdditionalFlagViewModel(EventAggregator eventAggregator, string flag)
+        {
+            this.eventAggregator = eventAggregator;
+            this.flag = flag;
+        }
+    }
+}
