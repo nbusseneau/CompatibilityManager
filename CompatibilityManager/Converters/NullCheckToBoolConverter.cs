f66f24aaf37b886bf72a1de7a25f475ec3650aa4
Skymirrh <skymirrh@gmail.com>
2018-02-09 19:39:56 +0100

    Add NullCheckToBoolConverter

---
+++ CompatibilityManager/Converters/NullCheckToBoolConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CompatibilityManager.Converters
+{
+    [ValueConversion(typeof(object), typeof(bool))]
+    public class NullCheckToBoolConverter : BaseConverter<NullCheckToBoolConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
